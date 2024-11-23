39a1d0c3847bba31f2d8aa100ecc781f5b86ad48
Caiyun Huang <114034228+hcaiyun@users.noreply.github.com>
2024-11-22 23:30:27 -0600

    Delete Login_page directory

--- Login_page/users.js
+++
@@ -1,9 +0,0 @@
-// users.js
-const users = [
-  { username: "admin", password: "admin123", role: "admin" },
-  { username: "customer1", password: "cust123", role: "customer" },
-  { username: "customer2", password: "cust456", role: "customer" },
-  { email: "123@gmail.com", username: "new", password: "new123", role: "customer" },
-];
-
-export default users;
